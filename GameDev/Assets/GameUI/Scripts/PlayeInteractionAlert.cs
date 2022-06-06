42a4fe8c061774a665c5f765ee43a9eb642848f0
Anastasia K <anastasia.kunst@gmx.de>
2022-06-06 03:17:56 +0200

    change items
    add new boots icons and weapon + checkpoint asset

--- GameDev/Assets/GameUI/Scripts/PlayeInteractionAlert.cs
+++ GameDev/Assets/GameUI/Scripts/PlayeInteractionAlert.cs
@@ -10,6 +10,9 @@ public class PlayeInteractionAlert : MonoBehaviour {
     public static bool ClollectAlertOpen = false;
     public GameObject ClollectAlertUI;                       //referenz set in editor
 
+    public static bool SaveAlertOpen = false;
+    public GameObject SaveAlertUI;                       //referenz set in editor
+
     /// <summary>
     /// Close the collect alert.
     /// </summary>
@@ -24,5 +27,18 @@ public class PlayeInteractionAlert : MonoBehaviour {
     public void OpenCollectAlertUi() {
         ClollectAlertUI.SetActive(true);
         ClollectAlertOpen = true;
+    }
+
+    public void CloseSaveAlertUi() {
+        SaveAlertUI.SetActive(false);
+        SaveAlertOpen = false;
+    }
+
+    /// <summary>
+    /// Open the collect alert.
+    /// </summary>
+    public void OpenSaveAlertUi() {
+        SaveAlertUI.SetActive(true);
+        SaveAlertOpen = true;
     }
 }
