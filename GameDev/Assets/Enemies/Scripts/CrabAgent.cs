7c6b18bf7db626000d630ea5be0e47b44348cdc3
Clemens <jan.clemens.weber-2@mni.thm.de>
2022-06-13 23:46:35 +0200

    enemie fixes

--- GameDev/Assets/Enemies/Scripts/CrabAgent.cs
+++ GameDev/Assets/Enemies/Scripts/CrabAgent.cs
@@ -182,9 +182,9 @@ public class CrabAgent : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if(collision.gameObject.tag == "Player")
+        if (other.tag == "Player")
         {
             doDamage = true;
         }
